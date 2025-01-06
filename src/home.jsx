e735c8a6a8e260c838c384260dc4159fa7c2ad4e
KNiC.xo <ntcaliwag@hotmail.com>
2025-01-06 07:44:21 -0800

    Reworked nav

--- src/home.jsx
+++ src/home.jsx
@@ -4,11 +4,18 @@ import './home.css';
 
 // Home page
 function Home() {
+  const [showDropdown, setShowDropdown] = useState(false);
+  function displayDropdown() {
+    setShowDropdown(prevState => !prevState);
+  }
+
   return (
     <>
-      <div className="top-nav">
+      {/* Header */}
+      <div className="header-container">
+        {/* Home Button */}
         <div className='home-button'>
-        <img id='home-button-img' src="/Portfolio-Website/coding.png" alt="" />
+          <img id='home-button-img' src="/Portfolio-Website/coding.png" alt=""/>
           <span id='home-button-name'>Nicholas Caliwag</span>
         </div>
 
@@ -21,7 +28,23 @@ function Home() {
             <a id='github-link' href="#">GitHub</a>
           </div>
         </div>
+
+
+        {/* Mobile Menu */}
+        <div className='menu-container' onClick={displayDropdown}>
+            <img id='menu' src="/Portfolio-Website/menu.png" alt="" />
+        </div>
       </div>
+
+      {/* Mobile Dropdown */}
+      {showDropdown &&       
+        <div className='dropdown'>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Projects</a></li>
+            <li><a href="#">Resume</a></li>
+          </ul>
+        </div>}
     </>
   );
 }
