ff4b3b407fba189fbec63d19ca7c6ca371820016
KNiC.xo <ntcaliwag@hotmail.com>
2025-03-10 16:23:06 -0700

    Added project video to modal gallery

--- src/projects/ImageSlider.jsx
+++ src/projects/ImageSlider.jsx
@@ -1,11 +1,11 @@
-import {useState} from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import './image-slider.css';
 
 // Changes current image displayed in container
 function ImageSlider(props) {
-  // Object array for all project images and videos
-  const projectGalleries = {
+  // Object array for all project images
+  const projectImages = {
     project1: [
       '/Portfolio-Website/weather-app/1.png', 
       '/Portfolio-Website/weather-app/2.png', 
@@ -14,25 +14,65 @@ function ImageSlider(props) {
     ]
   };
 
+  // Array for all project videos
+  const projectVideos = [
+    '/Portfolio-Website/weather-app/project1.mp4'
+  ];
+
+  // Tracks position in image array
   const [imgIndex, setImgIndex] = useState(0);
 
+
+  const videoRef = useRef(null);
+
+  // Displays next image in array
   function showNextImg() {
-    const arrayLength = projectGalleries[Object.keys(projectGalleries)[props.project]].length;
+    const arrayLength = projectImages[Object.keys(projectImages)[props.project]].length;
 
     // If within bounds, go to next image
-    if (imgIndex < arrayLength - 1) {
+    if (imgIndex < arrayLength) {
       setImgIndex(s => s + 1);
     }
   }
 
+  // Displays previous image in array
   function showPrevImg() {
-    const arrayLength = projectGalleries[Object.keys(projectGalleries)[props.project]].length;
+    const arrayLength = projectImages[Object.keys(projectImages)[props.project]].length;
 
     // If within bounds, go to previous image
     if (imgIndex > 0) {
       setImgIndex(s => s - 1);
     }
+
+    if (videoRef.current.style.transition == 'none') {
+      videoRef.current.style.transition = 'translate 300ms ease-in-out';
+    }
+
+    if (!videoRef.current.paused) {
+      videoRef.current.pause();
+    }
   }
+
+  // Adjusts video to be in the proper position so that fullscreen works correctly
+  const handleFullscreenChange = () => {
+    // Fix video position if in fullscreen
+    // Otherwise, set back to it's original position
+    if (document.fullscreenElement === videoRef.current) {
+      videoRef.current.style.transition = 'none'
+      videoRef.current.style.translate = '0%';
+    } else {
+      videoRef.current.style.translate = `${-100 * imgIndex}%`;
+    }
+  };
+
+  // Adds fullscreen event listener
+  useEffect(() => {
+    document.addEventListener('fullscreenchange', handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener('fullscreenchange', handleFullscreenChange);
+    };
+  }, [imgIndex]);
 
   return(
     <>
@@ -40,9 +80,11 @@ function ImageSlider(props) {
       <div className='slider-container'>
         {/* Gallery Images */}
         <div className='gallery-container'>
-          {projectGalleries[Object.keys(projectGalleries)[props.project]].map(url => {
-            return <img key={url}src={url} className='modal-img' style={{translate: `${-100 * imgIndex}%`}}/>
+          {projectImages[Object.keys(projectImages)[props.project]].map(url => {
+            return <img key={url} src={url} className='modal-img' style={{translate: `${-100 * imgIndex}%`}}/>
           })}
+          <video controls className='modal-video' ref={videoRef} src={projectVideos[props.project]} style={{translate: `${-100 * imgIndex}%`}}
+          ></video>
         </div>
 
         {/* Arrow Buttons */}
@@ -50,7 +92,7 @@ function ImageSlider(props) {
         <img onClick={showNextImg} className='arrow-button right-arrow' src="/Portfolio-Website/arrow.png" alt="" style={{right: 10}}/>
       </div>
     </>
-  )
+  );
 }
 
 export default ImageSlider;
